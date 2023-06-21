5e865a3e77807a4eb40ac5c97ee8d0e6a86c970e
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-20 23:46:02 -0500

    backend setup

--- App/back-end/app.js
+++ App/back-end/app.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express();
+
+// packages
+const cors = require('cors');
+
+// connect to db
+const connectDB = require('./db/connect');
 
 app.use(express.json());
 
@@ -10,6 +18,7 @@ app.get('/',(req,res)=>{
 const port = process.env.PORT || 5000
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, console.log(`Listening on ${port}...`))
     } catch (error) {
         console.log(error);
