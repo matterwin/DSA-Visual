00fef51fd9201abce478835e16d4ba5c402b6b61
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-24 01:51:47 -0500

    more

--- App/front-end/src/Core/Home/FileSys/children.js
+++ App/front-end/src/Core/Home/FileSys/children.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Children = ({ childName, isOpen }) => {
-  
+const Children = ({ childName, isOpen, childIndex, onClick, cIndex, setcIndex }) => {
+  const [focusedIndex, setFocusedIndex] = useState(false);
+
+  useEffect(() => {
+    setFocusedIndex(cIndex);
+  }, [cIndex]);
+
+  const handleChildClick = () => {
+    if (childIndex === focusedIndex) {
+      setFocusedIndex(false);
+      setcIndex(0);
+    } else {
+      setFocusedIndex(childIndex);
+      setcIndex(childIndex);
+    }
+    onClick(childIndex);
+  };
+
   return (
     isOpen && (
-      <div className='children-contain'>
+      <div className={`children-contain ${childIndex === focusedIndex ? 'clicked' : ''}`} onClick={handleChildClick}>
         <div className='children-content'>
           <div className='child1'>
             <p>{childName}</p>
