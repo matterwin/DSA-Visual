3f558d1fc454b84aabef69ac8e658cad52655441
Matthew Erwin <matthewderwin23@gmail.com>
2023-07-10 10:38:19 -0500

    next is backend

--- App/front-end/src/Core/Chat/Cats/tabBox.js
+++ App/front-end/src/Core/Chat/Cats/tabBox.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './tabBox.css'
 
-const TabBox = ({ title1, title2}) => {
+const TabBox = ({ title1, title2, title1Link, title2Link}) => {
     const [showTitle2, setShowTitle2] = useState(title2);
 
     useEffect(() => {
@@ -12,14 +12,14 @@ const TabBox = ({ title1, title2}) => {
   return (
     <div className='tab-box-contain'>
         <div className='box-div'>
-            <div className='tab-box-div'>
+            <a href={title1Link} className='tab-box-div'>
                 <p className='tab-box-p'>{title1}</p>
-            </div>
+            </a>
             {!showTitle2 && 
             <>
-                <div className='tab-box-div'>
+                <a href={title2Link} className='tab-box-div'>
                     <p className='tab-box-p'>{title2}</p>
-                </div>
+                </a>
             </>
             }
         </div>
